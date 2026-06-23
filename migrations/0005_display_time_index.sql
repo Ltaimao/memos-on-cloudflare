38e957adf49150ad497b68c5dec83def51badb91
scse <ltaimao@gmail.com>
2026-06-23 16:03:40 +0800

    Delete migrations/0005_display_time_index.sql

--- migrations/0005_display_time_index.sql
+++
@@ -1,6 +0,0 @@
--- 为 displayTime 查询创建索引（不包含 creator_id，用于全局日期过滤）
-
-CREATE INDEX idx_memo_created_ts ON memo(created_ts DESC);
-
--- 为用户级时间查询创建组合索引
-CREATE INDEX idx_memo_creator_created_ts ON memo(creator_id, created_ts DESC);
