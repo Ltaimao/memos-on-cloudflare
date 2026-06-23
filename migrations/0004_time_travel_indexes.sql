2ade8746c7deeb11552d3d6241d207a0b1463c1f
scse <ltaimao@gmail.com>
2026-06-23 16:02:54 +0800

    Update 0004_time_travel_indexes.sql

--- migrations/0004_time_travel_indexes.sql
+++ migrations/0004_time_travel_indexes.sql
@@ -27,3 +27,6 @@ CREATE INDEX idx_memo_creator_year_month_weekday ON memo(creator_id, year_month_
 
 -- 4. 为 displayTime 查询创建索引（不包含 creator_id，用于全局日期过滤）
 CREATE INDEX idx_memo_created_ts ON memo(created_ts DESC);
+
+-- 为用户级时间查询创建组合索引
+CREATE INDEX idx_memo_creator_created_ts ON memo(creator_id, created_ts DESC);
